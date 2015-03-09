e1d998a038ded5f4d209eaf0da40977f9aa39dcd
chen <626239153@qq.com>
2015-03-09 18:09:23 +0800

    reinit
    之前上传没有project

---
+++ refreshDataTests/refreshDataTests.m
@@ -0,0 +1,34 @@
+//
+//  refreshDataTests.m
+//  refreshDataTests
+//
+//  Created by chen on 14-5-29.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface refreshDataTests : XCTestCase
+
+@end
+
+@implementation refreshDataTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
